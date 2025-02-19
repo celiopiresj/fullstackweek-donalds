d6f123c85a17d39044a7fe699d67427e3a665e48
Celio Pires Jr. <72475635+celiopiresj@users.noreply.github.com>
2025-02-19 17:38:04 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./componets/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+  if (!restaurant) return notFound();
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center px-6">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant?.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant?.name}</h2>
+      </div>
+      <div className="space-y-2 pt-10 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
+          imageUrl="/dine_in.png"
+          imageAlt="Para comer aqui"
+          buttonText="Para comer aqui"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
+          imageUrl="/takeaway.png"
+          imageAlt="Para levar"
+          buttonText="Para levar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
